8304c926b9ad5841e49212e12928df3b2fba3171
dev_bpn <jazzbpn@gmail.co m>
2016-06-28 11:49:10 +0545

    initial commit 2

--- app/src/main/java/parceableandserialization/bpn/com/parceableandserialization/MainActivity.java
+++ app/src/main/java/parceableandserialization/bpn/com/parceableandserialization/MainActivity.java
@@ -1,18 +1,12 @@
 package parceableandserialization.bpn.com.parceableandserialization;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,32 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
 
-        et = (EditText) findViewById(R.id.et);
+
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     private void fabTask(){
 
@@ -63,26 +35,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                clickedJobs();
 
             }
         });
 
     }
 
-    private void clickedJobs() {
 
-        // Creating an instance of Student class with user input data
-        Student student = new Student(et.getText().toString());
-
-        // Creating an intent to open the activity StudentViewActivity
-        Intent intent = new Intent(getBaseContext(), Activity2.class);
-
-        // Passing data as a parecelable object to StudentViewActivity
-        intent.putExtra("student",student);
-
-        // Opening the activity
-        startActivity(intent);
-
-    }
 }
